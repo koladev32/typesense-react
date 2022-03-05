332bccae45de43e612d02ea79a77c7586b68bad1
koladev <onaelmangabo@gmail.com>
2022-03-05 05:46:05 +0100

    - prettitying

--- import.js
+++ import.js
@@ -1,51 +1,54 @@
 const Typesense = require("typesense");
 
-
 async function main() {
   let client = new Typesense.Client({
     apiKey: "xyz",
-    nodes: [{
-      host: "localhost",
-      port: "8108",
-      protocol: "http",
-    }],
+    nodes: [
+      {
+        host: "localhost",
+        port: "8108",
+        protocol: "http",
+      },
+    ],
     connectionTimeoutSeconds: 5,
   });
 
   let animeSchema = {
-    'name': 'animes',
-    'fields': [
+    name: "animes",
+    fields: [
       {
-        'name': 'title',
-        'type': 'string',
-        'facet': true,
+        name: "title",
+        type: "string",
+        facet: true,
       },
       {
-        'name': 'tags',
-        'type': 'string[]',
-        'facet': true,
+        name: "tags",
+        type: "string[]",
+        facet: true,
       },
       {
-        'name': 'sources',
-        'type': 'string[]',
-        'facet': true,
+        name: "sources",
+        type: "string[]",
+        facet: true,
       },
       {
-        'name': 'type',
-        'type': 'string',
+        name: "type",
+        type: "string",
       },
       {
-        'name': 'episodes',
-        'type': 'int32',
+        name: "episodes",
+        type: "int32",
       },
       {
-        name: 'status',
-        type: 'string',
-      }
-    ]
-    };
+        name: "status",
+        type: "string",
+      },
+    ],
+  };
 
-  client.collections().create(animeSchema)
+  client
+    .collections()
+    .create(animeSchema)
     .then(function (response) {
       console.log(response);
     })
@@ -53,24 +56,23 @@ async function main() {
       console.log(error);
     });
 
-  let fs = require('fs/promises');
+  let fs = require("fs/promises");
 
-  const animesInJsonL = await fs.readFile('anime.jsonl', 'utf8');
-  client.collections('animes').documents().import(animesInJsonL);
+  const animesInJsonL = await fs.readFile("anime.jsonl", "utf8");
+  client.collections("animes").documents().import(animesInJsonL);
 
   let searchParameters = {
-    'q': 'Parade',
-    'query_by': 'title',
-  }
+    q: "Parade",
+    query_by: "title",
+  };
 
-  client.collections('animes')
+  client
+    .collections("animes")
     .documents()
     .search(searchParameters)
     .then(function (searchResults) {
-      console.log(searchResults.hits)
-    })
+      console.log(searchResults.hits);
+    });
 }
-
-
 
 main();
--- src/App.js
+++ src/App.js
@@ -1,47 +1,49 @@
-import './App.css';
-import TypesenseInstantsearchAdapter from 'typesense-instantsearch-adapter';
-import { InstantSearch } from 'react-instantsearch-dom';
-import { Hits } from 'react-instantsearch-dom';
-import { Hit } from './components/Card';
-import CustomSearchBox from './components/SearchBox';
-import { RefinementList } from 'react-instantsearch-dom';
-import { Configure } from 'react-instantsearch-dom';
-import { Pagination } from 'react-instantsearch-dom';
+import "./App.css";
+import TypesenseInstantsearchAdapter from "typesense-instantsearch-adapter";
+import { InstantSearch } from "react-instantsearch-dom";
+import { Hits } from "react-instantsearch-dom";
+import { Hit } from "./components/Card";
+import CustomSearchBox from "./components/SearchBox";
+import { RefinementList } from "react-instantsearch-dom";
+import { Configure } from "react-instantsearch-dom";
+import { Pagination } from "react-instantsearch-dom";
 
 const typesenseInstanrSearchAdapter = new TypesenseInstantsearchAdapter({
   server: {
-    apiKey: 'xyz',
+    apiKey: "xyz",
     nodes: [
       {
-        host: 'localhost',
-        port: '8108',
-        protocol: 'http',
-      }
-    ]
+        host: "localhost",
+        port: "8108",
+        protocol: "http",
+      },
+    ],
   },
   additionalSearchParameters: {
     query_by: "title",
   },
-})
-
+});
 
 function App() {
   return (
     <div>
-      <InstantSearch indexName='animes' searchClient={typesenseInstanrSearchAdapter.searchClient}>
+      <InstantSearch
+        indexName="animes"
+        searchClient={typesenseInstanrSearchAdapter.searchClient}
+      >
         <Configure hitsPerPage={9} />
         <div className="flex flex-row">
-        <RefinementList className="mt-8" attribute="tags" />
-        <div className='flex flex-col space-y-3 w-10/12'>
-          <div className='text-center text-2xl font-bold  text-blue-700'>
-            Search for animes here
+          <RefinementList className="mt-8" attribute="tags" />
+          <div className="flex flex-col space-y-3 w-10/12">
+            <div className="text-center text-2xl font-bold  text-blue-700">
+              Search for animes here
+            </div>
+            <div className="self-center w-1/4">
+              <CustomSearchBox />
+            </div>
+            <Hits hitComponent={Hit} />
+            <Pagination className="flex flex-row justify-center mb-10" />
           </div>
-          <div className='self-center w-1/4'>
-            <CustomSearchBox />
-          </div>
-          <Hits hitComponent={Hit} />
-          <Pagination className="flex flex-row justify-center mb-10" />
-        </div>
         </div>
       </InstantSearch>
     </div>
